ee6983d113f9bb846998d12d38e6eab0c978d77c
Iacobucci Juan B <32987645+IacobucciB@users.noreply.github.com>
2022-10-15 20:02:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-console.log("Hello")
 
+const obserber = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+        
+        console.log("ENTRY");
+        
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+
+    });
+
+
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+
+hiddenElements.forEach((el) => obserber.observe(el))
