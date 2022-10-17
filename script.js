d470322327d9c3165868db90bfd4039de371e57b
Iacobucci Juan B <link00222@gmail.com>
2022-10-17 11:16:51 -0300

    Indentacion de codigo

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ console.log("Hello World")
 const obserber = new IntersectionObserver((entries) => {
 
     entries.forEach((entry) => {
-        
+
         console.log("ENTRY");
-        
-        if(entry.isIntersecting) {
+
+        if (entry.isIntersecting) {
             entry.target.classList.add('show');
         } else {
             entry.target.classList.remove('show');
