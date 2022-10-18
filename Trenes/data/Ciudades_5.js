c61e45a6dd08d8e7f0ff1a44747bb3516c6dacbf
Argos2365 <110508219+Argos2365@users.noreply.github.com>
2022-10-18 01:40:55 -0300

    Add files via upload

---
+++ Trenes/data/Ciudades_5.js
@@ -0,0 +1,16 @@
+var json_Ciudades_5 = {
+"type": "FeatureCollection",
+"name": "Ciudades_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Bahia Blanca" }, "geometry": { "type": "Point", "coordinates": [ -62.291881015112445, -38.725870560249113 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "La Plata" }, "geometry": { "type": "Point", "coordinates": [ -58.05664958110804, -34.915797494137045 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "C.A.B.A" }, "geometry": { "type": "Point", "coordinates": [ -58.48180794900037, -34.621457085596198 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Rosario" }, "geometry": { "type": "Point", "coordinates": [ -60.689361013056718, -32.969880348783676 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Cordoba" }, "geometry": { "type": "Point", "coordinates": [ -64.172389180790077, -31.432769326403694 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "San Juan" }, "geometry": { "type": "Point", "coordinates": [ -68.423972859713402, -31.555411163295719 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Mendoza" }, "geometry": { "type": "Point", "coordinates": [ -68.751017758092118, -32.912647491567398 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Bariloche" }, "geometry": { "type": "Point", "coordinates": [ -71.160929853020292, -41.131694593947543 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nombre": "San Miguel De Tucuman" }, "geometry": { "type": "Point", "coordinates": [ -65.169876120845089, -26.856184779716294 ] } }
+]
+}
