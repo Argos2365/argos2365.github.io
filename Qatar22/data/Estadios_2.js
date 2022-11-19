2170a17dd2f14828793495346b16324216d354a2
Argos2365 <110508219+Argos2365@users.noreply.github.com>
2022-11-19 06:31:38 -0300

    Add files via upload

---
+++ Qatar22/data/Estadios_2.js
@@ -0,0 +1,15 @@
+var json_Estadios_2 = {
+"type": "FeatureCollection",
+"name": "Estadios_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "Nombre": "Estadio Al Bayt", "Capacidad": "60 000 espectadores", "Partidos": "Inauguracion, Octavos, Cuartos, y Semis", "Coste": "674 000 000 USD" }, "geometry": { "type": "Point", "coordinates": [ 51.4875, 25.652222 ] } },
+{ "type": "Feature", "properties": { "fid": "2", "Nombre": "Estadio 974", "Capacidad": "40 000 espectadores", "Partidos": "Grupos y Octavos", "Coste": "Desconocido" }, "geometry": { "type": "Point", "coordinates": [ 51.566465, 25.288695 ] } },
+{ "type": "Feature", "properties": { "fid": "3", "Nombre": "Estadio Al Janoub", "Capacidad": "40 000​ espectadores", "Partidos": "Grupos Y Octavos De Final", "Coste": "286.000.000 USD" }, "geometry": { "type": "Point", "coordinates": [ 51.574083, 25.159778 ] } },
+{ "type": "Feature", "properties": { "fid": "4", "Nombre": "Estadio Áhmad bin Ali", "Capacidad": "60 000 espectadores", "Partidos": "Grupos y Octavos.", "Coste": "Desconocido" }, "geometry": { "type": "Point", "coordinates": [ 51.341279, 25.330538 ] } },
+{ "type": "Feature", "properties": { "fid": "5", "Nombre": "Estadio Education CIty", "Capacidad": "40 000 espectadores", "Partidos": "Grupos Octavos y Cuartos De Final", "Coste": "Desconocido" }, "geometry": { "type": "Point", "coordinates": [ 51.424274, 25.310595 ] } },
+{ "type": "Feature", "properties": { "fid": "6", "Nombre": "Estadio Khalifa International", "Capacidad": "45,416 espectadores", "Partidos": "Grupos Octavos y 3er Puesto", "Coste": "Desconocido" }, "geometry": { "type": "Point", "coordinates": [ 51.448056, 25.263611 ] } },
+{ "type": "Feature", "properties": { "fid": "7", "Nombre": "Estadio Al Thumama", "Capacidad": "40 000 espectadores", "Partidos": "Grupos Octavos y Cuartos", "Coste": "Desconocido" }, "geometry": { "type": "Point", "coordinates": [ 51.532113, 25.235052 ] } },
+{ "type": "Feature", "properties": { "fid": "8", "Nombre": "Estadio Icónico de Lusail", "Capacidad": "80 000 espectadores", "Partidos": "Grupos, Octavos, Cuartos, Semis Y Final", "Coste": "2 105 000 000 USD " }, "geometry": { "type": "Point", "coordinates": [ 51.490389, 25.420861 ] } }
+]
+}
